c24134513920e4b02a5aa64564da5f4abcdf934d
Shinya Todaka <tdk0105.strawberry@gmail.com>
2020-09-07 23:42:52 +0900

    fix

--- functions/index.ts
+++ functions/index.ts
@@ -1,15 +1,26 @@
 import * as functions from 'firebase-functions';
 import * as path from 'path';
+import express from 'express';
 import next from 'next';
 
-const distDir = `${path.relative(process.cwd(), __dirname)}/next`;
-
-const nextjsServer = next({
-  dev: false,
-  conf: { distDir },
+const helloWorld = express();
+helloWorld.get('*', (req, res) => {
+  res.status(200).send('Hello World!');
 });
-const nextjsHandle = nextjsServer.getRequestHandler();
 
-export const nextjsFunc = functions.https.onRequest((req, res) => {
-  return nextjsServer.prepare().then(() => nextjsHandle(req, res));
+const server = express();
+server.use('/helloWorld', helloWorld);
+server.get('*', async (_req, _res) => {
+  const distDir = `${path.relative(process.cwd(), __dirname)}/next`;
+
+  functions.logger.info('distDir', distDir);
+  const nextjsServer = next({
+    dev: false,
+    conf: { distDir },
+  });
+  const nextjsHandle = nextjsServer.getRequestHandler();
+  await nextjsServer.prepare();
+  await nextjsHandle(_req, _res);
 });
+
+export const nextjsFunc = functions.https.onRequest(server);
