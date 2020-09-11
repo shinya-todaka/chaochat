9ff4e565dee4143cc7a0689046ad533811fcded1
Shinya Todaka <tdk0105.strawberry@gmail.com>
2020-09-12 08:29:37 +0900

    fix

--- functions/tasks/index.ts
+++ functions/tasks/index.ts
@@ -7,7 +7,9 @@ export const firestoreTtlCallback = functions
   .https.onRequest(async (req, res) => {
     const payload = req.body as ExpirationTaskPayload;
     try {
-      await admin.firestore().doc(payload.docPath).update({ isClosed: true });
+      await admin.firestore().doc(payload.docPath).update({
+        isClosed: true,
+      });
       res.send(200);
     } catch (error) {
       functions.logger.error(error);
